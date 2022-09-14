4e8d644b44d7ecf91920aed9d2999d17d0d122e3
Truong An Nguyen Si <nguyensitruongan@gmail.com>
2022-09-14 11:22:52 +0700

    aaaaaaaa

--- AppTinhLuong365/Views/PageNhapLuongCoBanVaCheDo.xaml.cs
+++ AppTinhLuong365/Views/PageNhapLuongCoBanVaCheDo.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -58,6 +59,95 @@ namespace AppTinhLuong365.Views
                 DockPanel.SetDock(wrapLCBVCD, Dock.Right);
             else
                 DockPanel.SetDock(wrapLCBVCD, Dock.Bottom);
+        }
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Process.Start("https://phanmemnhansu.timviec365.vn/bien-dong-nhan-su.html");
+        }
+        public class abc : INotifyPropertyChanged
+        {
+            public event PropertyChangedEventHandler PropertyChanged;
+            protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+            public string name { get; set; }
+            private int _hover;
+            public int hover
+            {
+                get => _hover;
+                set
+                {
+                    _hover = value;
+                    OnPropertyChanged();
+                }
+            }
+
+        }
+        public List<abc> Test { get; set; } = new List<abc>() { new abc { name = "aa" }, new abc { name = "bb" }, new abc { name = "cc" } };
+
+        private void Border_MouseLeftButtonDown_1(object sender, MouseButtonEventArgs e)
+        {
+            Main.PopupSelection.NavigationService.Navigate(new Views.NhapLuongCoBan(Main));
+            Main.PopupSelection.Visibility = Visibility.Visible;
+            Main.PopupSelection.Width = 500;
+            Main.PopupSelection.Height = 500;
+        }
+
+        private void Border_MouseLeftButtonDown_2(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+        private void dataGrid1Hover(object sender, MouseEventArgs e)
+        {
+            Border col = sender as Border;
+            if (col != null)
+            {
+                abc item = (abc)col.DataContext;
+                int index = Test.FindIndex(x => x.name == item.name);
+                if (index > -1)
+                {
+                    Test[index].hover = 1;
+                }
+            }
+            else if ((sender as Grid) != null)
+            {
+                abc item = (abc)(sender as Grid).DataContext;
+                int index = Test.FindIndex(x => x.name == item.name);
+                if (index > -1)
+                {
+                    Test[index].hover = 1;
+                }
+            }
+        }
+
+        private void dataGrid1Leave(object sender, MouseEventArgs e)
+        {
+            Border col = sender as Border;
+            if (col != null)
+            {
+                abc item = (abc)col.DataContext;
+                int index = Test.FindIndex(x => x.name == item.name);
+                if (index > -1)
+                {
+                    Test[index].hover = 0;
+                }
+            }
+            else if ((sender as Grid) != null)
+            {
+                abc item = (abc)(sender as Grid).DataContext;
+                int index = Test.FindIndex(x => x.name == item.name);
+                if (index > -1)
+                {
+                    Test[index].hover = 0;
+                }
+            }
+        }
+
+        private void DockPanel_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
         }
     }
 }
