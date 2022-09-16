735d4ec2eeba0e9d4c346c7984b20435889df4f2
Vietcuong0508 <cuongtvth2009009@fpt.edu.vn>
2022-09-16 16:13:53 +0700

    aaaa

---
+++ AppTinhLuong365/Views/TinhLuong/ChiTietLuong.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppTinhLuong365.Views.TinhLuong
+{
+    /// <summary>
+    /// Interaction logic for ChiTietLuong.xaml
+    /// </summary>
+    public partial class ChiTietLuong : Page, INotifyPropertyChanged
+    {
+        private int _IsSmallSize;
+        public int IsSmallSize
+        {
+            get { return _IsSmallSize; }
+            set { _IsSmallSize = value; OnPropertyChanged("IsSmallSize"); }
+        }
+        public MainWindow Main;
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public ChiTietLuong(MainWindow main)
+        {
+            ItemList = new ObservableCollection<string>();
+            for (var i = 1; i <= 12; i++)
+            {
+                ItemList.Add($"Tháng {i}");
+            }
+            YearList = new ObservableCollection<string>();
+            for (var i = 2022; i <= 2025; i++)
+            {
+                YearList.Add($"Năm {i}");
+            }
+            InitializeComponent();
+            this.DataContext = this;
+            Main = main;
+        }
+        public ObservableCollection<string> ItemList { get; set; }
+        public ObservableCollection<string> YearList { get; set; }
+
+        private void Page_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (this.ActualWidth > 980)
+            {
+                IsSmallSize = 0;
+            }
+            else if (this.ActualWidth <= 980 && this.ActualWidth > 460)
+            {
+                IsSmallSize = 1;
+            }
+            else /*(this.ActualWidth <= 460)*/
+            {
+                IsSmallSize = 2;
+            }
+        }
+    }
+}
